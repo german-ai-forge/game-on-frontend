2796b07fa1c657bc0552a5ce55e53b2e38a65cd3
german rodriguez <rodriguez.german00@gmail.com>
2026-05-14 20:07:23 -0300

    Prepare frontend for Streamlit Cloud deployment

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 #API_BASE_URL = "https://game-on-frontend.run.app"
+import os
 
-API_BASE_URL = "http://127.0.0.1:8000"
+API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
 QUERY_API_ENDPOINT = f"{API_BASE_URL}/query"
