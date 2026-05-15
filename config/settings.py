6a4533715cb367a1181508fd3e43287599b97c1d
Reynaldo Huaman Valenzuela <68795117+zxrey@users.noreply.github.com>
2026-05-14 19:23:15 -0500

    url

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,4 @@
 API_BASE_URL = "https://gameon-api-790173096042.southamerica-west1.run.app"
 
-API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
+#API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
 QUERY_API_ENDPOINT = f"{API_BASE_URL}/query"
